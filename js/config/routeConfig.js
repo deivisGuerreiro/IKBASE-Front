582e8fe28c596e571b713a18bfff0a39bc0a2012
Luis Felipe <lipevasconcelos231@gmail.com>
2019-12-18 11:53:14 -0300

    html/css usuario

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -7,6 +7,10 @@ angular.module('ikbaseApp').config(function ($routeProvider) {
     $routeProvider.when('/feed', {
         templateUrl: './view/Feed/feed.html',
         controller: 'feedController'
+    });
+    $routeProvider.when('/user', {
+        templateUrl: './view/User/profile.html',
+        controller: 'usuarioController'
     })
     $routeProvider.otherwise({ redirectTo: '/login' });
 })
